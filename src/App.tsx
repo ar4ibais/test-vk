550b9e02a3a74a5884ac34286685783197f25f8e
ar4ibais <ak.hutov@yandex.ru>
2024-03-07 21:16:54 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./scss/reset.scss";
+import "./scss/style.scss";
+
+function App() {
+	return (
+		<>
+			<div className="main">
+				<h1>Test Vk Application</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
